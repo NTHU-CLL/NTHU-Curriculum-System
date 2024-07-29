36e56999c8aa6d5dee3d14c806b69058b15d888a
Connection <69660530+ConnectionOuOb@users.noreply.github.com>
2024-07-29 18:30:53 +0800

    Make responsive device detect & Sync to Provider

--- version_1/lib/config.dart
+++ version_1/lib/config.dart
@@ -6,7 +6,27 @@ import 'tab/interdisciplinary.dart';
 import 'package:flutter/material.dart';
 
 // ##### Setting config
-// ### Color (Light theme)
+// ### System
+// # device threshold
+const double mobileThreshold = 850;
+
+// # Language
+const List<Locale> supportedLocales = [
+  Locale('zh', 'TW'),
+  Locale('en', 'US'),
+];
+
+// # Page route
+Map<String, Widget> mainPages = {
+  'tab_search': const PageSearchClass(),
+  'tab_rule': const PagePrerequisite(),
+  'tab_credit': const PageCreditProgram(),
+  'tab_interdisciplinary': const PageInterdisciplinary(),
+  'tab_map': const PageMap(),
+};
+
+// ### UI
+// # Light theme
 const Color lightBackground = Colors.white;
 const Color lightError = Color.fromRGBO(243, 13, 13, 1);
 const Color lightPrimary1 = Color.fromRGBO(181, 42, 203, 1);
@@ -20,33 +40,25 @@ const Color lightComponent5 = Color.fromRGBO(71, 192, 29, 1);
 const Color lightComponent6 = Color.fromRGBO(138, 19, 143, 1);
 const Color lightComponent7 = Color.fromRGBO(233, 85, 161, 1);
 
-// ### Color (Dark theme)
+// # Dark theme
 const Color darkBackground = Color.fromRGBO(42, 42, 42, 1);
 const Color darkError = Color.fromRGBO(243, 13, 13, 1);
 const Color darkPrimary1 = Color.fromRGBO(141, 40, 145, 1);
 const Color darkPrimary2 = Color.fromRGBO(17, 97, 39, 1);
 const Color darkSecondary = Color.fromRGBO(231, 153, 193, 1);
 
-// ### Font
+// # Font
 const double fontH2 = 32;
 const double fontH3 = 24;
 const double fontH4 = 20;
 const double fontRegular = 16;
 
-// ### Radius
+// # Radius
 BorderRadius radiusButton = BorderRadius.circular(50);
 
-// ### Language
-const List<Locale> supportedLocales = [
-  Locale('zh', 'TW'),
-  Locale('en', 'US'),
-];
+// ### Enum
+// # Device screen
+enum DeviceScreen { mobile, tablet, desktop }
 
-// ### Page route
-Map<String, Widget> mainPages = {
-  'tab_search': const PageSearchClass(),
-  'tab_rule': const PagePrerequisite(),
-  'tab_credit': const PageCreditProgram(),
-  'tab_interdisciplinary': const PageInterdisciplinary(),
-  'tab_map': const PageMap(),
-};
+// # Language
+enum SystemLanguage { chinese, english }
